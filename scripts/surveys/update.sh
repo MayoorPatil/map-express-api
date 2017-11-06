d118b103721f3d0cabeb3e8cf8ee8ede7f6b62aa
mayoorpatil <mayoor.patil@libertymutual.com>
2017-11-06 15:32:32 -0500

    Modified the update script to update all questions

--- scripts/surveys/update.sh
+++ scripts/surveys/update.sh
@@ -9,7 +9,26 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
-    "active": false
+    "survey": {
+      "title": "lets Go!",
+      "questions": [{
+        "question1": {
+          "questionDescription": "Was this a good book?",
+          "active": false,
+          "options": ["Strongly Agree", "NEUTRAL", "Disagree"]
+        },
+        "question2": {
+          "questionDescription": "Was the length of the book what you expected?",
+          "active": false,
+          "options": ["Strongly Agree", "Neutral", "DISAGREE"]
+        },
+        "question3": {
+          "questionDescription": "Were the illustrations well done?",
+          "active": false,
+          "options": ["Strongly Agree", "NEUTRAL", "Disagree"]
+        }
+      }]
+    }
   }'
 
 echo
